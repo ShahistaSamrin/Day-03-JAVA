24ce0fdae6d828f74c65883ebb1fd6fefc655a9a
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-04 12:35:28 +0530

    Add files via upload

---
+++ ifelseevenodd.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ifelseevenodd{
+    public static void main(String args[]){
+        int a=8388608;
+        if((a&1)==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+    }
+}
